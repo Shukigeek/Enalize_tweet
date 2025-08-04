09f047bf9a404996f761e79947442958cea313b7
shuki <shukiheftler@gmail.com>
2025-08-04 13:00:27 +0300

    added func that return the three longest tweet on each category

--- src/data_analyze.py
+++ src/data_analyze.py
@@ -9,16 +9,14 @@ class DataAnalyzer:
     # loading data at initialize
     def __init__(self,data:DataFrame):
         self.df = data
+        self.anti = self.df[self.df['Biased'] == 1].copy()
+        self.not_anti = self.df[self.df['Biased'] == 0].copy()
+        self.undefined = self.df[~self.df['Biased'].isin([0, 1])].copy()
 
     # counting the tweet in each category
     def category_count(self):
-        self.anti = self.df[self.df['Biased'] == 1]
         self.anti_sum = int(self.anti['Biased'].count())
-
-        self.not_anti = self.df[self.df['Biased'] == 0]
         self.not_anti_sum = int(self.not_anti['Biased'].count())
-
-        self.undefined = self.df[~self.df['Biased'].isin([0,1])]
         self.undefined_sum = int(self.undefined['Biased'].count())
 
         return {
@@ -39,7 +37,30 @@ class DataAnalyzer:
             'undefined tweet len': self.avg_len_undefined
             }
 
+    # find the 3 longest tweet by category
+    def three_longest_tweets(self):
+        self.anti['len'] = self.anti['Text'].apply(lambda x : len(str(x)))
+        self.three_longest_anti = self.anti.sort_values(by=['len'],ascending=False)[0:3]
+
+
+        self.not_anti['len'] = self.not_anti['Text'].apply(lambda x : len(str(x)))
+        self.three_longest_not_anti = self.not_anti.sort_values(by=['len'],ascending=False)[0:3]
+
+
+        self.undefined['len'] = self.undefined['Text'].apply(lambda x : len(str(x)))
+        self.three_longest_undefined = self.undefined.sort_values(by=['len'],ascending=False)[0:3]
+
+
+        return {
+            'longest three tweets': {
+                    'antisemitic':self.three_longest_anti['Text'].tolist(),
+                    'not antisemitic': self.three_longest_not_anti['Text'].tolist(),
+                    'undefined':self.three_longest_undefined['Text'].tolist()
+                    }
+        }
+
 
 a = DataAnalyzer(df)
 print(a.category_count())
 print(a.average_tweet())
+print(a.three_longest_tweets())
